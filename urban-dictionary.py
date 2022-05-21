b5e9bd6533e8a71ae4c155e224a3afb3c0b0b63b
Igor Sterner <igorsterner@gmail.com>
2022-05-21 16:01:45 +0100

    "Urban Dictionary efficient processing"

--- urban-dictionary.py
+++ urban-dictionary.py
@@ -1,35 +1,74 @@
 from tqdm import tqdm
 from collections import Counter
+import pprint
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+entries = ""
 
-entries = []
-dictionary = []
+# with open(f"dictionaries/urban-dictionary/data/A.data", 'r', encoding='utf-8') as f:
+#     entries = f.read().replace('\n', ' ')
 
-print("Gathering all terms in Urban Dictionary")
+with open("dictionaries/100k-en.txt", 'r', encoding='utf-8') as f:
+    english = f.read().splitlines()
 
 for char in alphabet[0:26]:
     with open(f"dictionaries/urban-dictionary/data/{char.upper()}.data", 'r', encoding = 'utf-8') as f:
-        entries += f.read().splitlines()
+        entries += f.read().replace('\n', ' ').replace('"','')
 
-print(f"Finished gathering {len(entries)} terms in Urban Dictionary")
+print(len(entries))
 
-print("Writing distinct words")
+print("Splitting words...")
+words = entries.split()
 
-with open("dictionaries/urban.txt", 'w', encoding = 'utf-8') as w:
-    for entry in tqdm(entries):
-        entry = entry.replace('"', '')
-        for word in entry.split():
-            save = True
-            lower_word = word.lower()
-            if word.isalpha():
+print("Passing words to instance of Counter class...")
+Counter = Counter(words)
 
-            if save:
-                if lower_word not in dictionary:
-                    dictionary.append(lower_word)
-                    w.write(lower_word)
-                    w.write('\n')
+print("Finding most common words...")
+top_words = Counter.most_common(100000)
 
-print(f"Finished saving {len(dictionary)} distinct words from the Urban Dictionary")
+urban = []
+for word in tqdm(top_words):
+    if word not in english:
+        urban.append(word)
+
+print(urban)
+print(len(urban))
+
+print("Done")
+
+# print(type(top_words))
+# pprint.pprint(top_words)
+
+
+# alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+#
+# entries = []
+# dictionary = []
+#
+# print("Gathering all terms in Urban Dictionary")
+#
+# for char in alphabet[0:26]:
+#     with open(f"dictionaries/urban-dictionary/data/{char.upper()}.data", 'r', encoding = 'utf-8') as f:
+#         entries += f.read().splitlines()
+#
+# print(f"Finished gathering {len(entries)} terms in Urban Dictionary")
+#
+# print("Writing distinct words")
+#
+# with open("dictionaries/urban.txt", 'w', encoding = 'utf-8') as w:
+#     for entry in tqdm(entries):
+#         entry = entry.replace('"', '')
+#         for word in entry.split():
+#             save = True
+#             lower_word = word.lower()
+#             if word.isalpha():
+#
+#             if save:
+#                 if lower_word not in dictionary:
+#                     dictionary.append(lower_word)
+#                     w.write(lower_word)
+#                     w.write('\n')
+#
+# print(f"Finished saving {len(dictionary)} distinct words from the Urban Dictionary")
 # print(dictionary[0:1000])
 # print(len(dictionary))
