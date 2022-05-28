1507e1c5e55a0371f10293ae7b0844bb4413be20
Igor Sterner <igorsterner@gmail.com>
2022-05-28 15:50:18 +0100

    code-switching in tweets checker

--- csw.py
+++ csw.py
@@ -1,9 +1,19 @@
 import pprint
+import nltk
+from cswdictionary import csv_to_words
 
 with open("corpora/clean-tweets/clean-tweets-2022-04.txt", 'r', encoding = 'utf-8') as f:
     tweets = f.read().splitlines()
 
-tweets = tweets[0:10]
+words_tup = csv_to_words('dictionaries/csw.csv')
+words = [i[0] for i in words_tup]
+words.remove("URL")
+tweets = tweets[0:30]
 
 for tweet in tweets:
-    print(tweet)
+    for word in nltk.word_tokenize(tweet):
+        if word in words:
+            print(word)
+            print(tweet)
+            print("-------")
+            break
