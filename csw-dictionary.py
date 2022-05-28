53b67f29d9a83ed16bde3dd56a8fb2146277fe3d
Igor Sterner <igorsterner@gmail.com>
2022-05-28 15:37:54 +0100

    csw dictionary created

--- csw-dictionary.py
+++ csw-dictionary.py
@@ -1,9 +1,5 @@
 import csv
-import pprint
-from collections import defaultdict
-import pandas as pd
-from operator import itemgetter
-
+from tqdm import tqdm
 
 # df = pd.read_csv('dictionaries/en-dict.csv')
 # words = df.the
@@ -15,7 +11,6 @@ def csv_to_words(file):
     with open(file, 'r', encoding='utf-8') as f:
         reader = csv.reader(f)
         for row in reader:
-            print(row)
             lan.append((row[0], int(row[1])))
     return lan
 
@@ -24,9 +19,37 @@ english = csv_to_words('dictionaries/en-dict.csv')
 german = csv_to_words('dictionaries/de-dict.csv')
 urban = csv_to_words('dictionaries/urban.csv')
 
-print(english[0:10])
-print(urban[0:10])
-eng_tot = english + german
-print(eng_tot[0:10])
-eng_tot = sorted(eng_tot,key=itemgetter(1))
-print(eng_tot[0:10])
+eng_tot = english + urban
+
+eng = {}
+
+for word in tqdm(eng_tot):
+    if word[0] in eng:
+        eng[word[0]] += word[1]
+    else:
+        eng[word[0]] = word[1]
+
+
+eng = sorted(eng.items(), key=lambda item: item[1], reverse = True)
+
+tot = {}
+
+german = dict(german)
+for word in eng:
+    if word[0] not in german:
+        tot[word[0]] = word[1]
+
+i = 0
+j = 0
+file = "dictionaries/csw.csv"
+with open(file, 'w', encoding='utf-8') as f:
+    for word in tot.keys():
+        j += 1
+        i += tot[word]
+        f.write(f"{word},{tot[word]}\n")
+
+print(f"Outputted file {file} with {j} terms with word frequencies out of {i}")
+
+
+# eng_tot = list(dict(eng_tot).items())
+# eng_tot = sorted(eng_tot, key=lambda x: x[1], reverse = True)
--- urban-dictionary.py
+++ urban-dictionary.py
@@ -4,9 +4,6 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
             'v', 'w', 'x', 'y', 'z', ' ']
 entries = ""
 
-# with open("dictionaries/en-dict.txt", 'r', encoding='utf-8') as f:
-#     english = f.read().splitlines()
-
 for char in alphabet[0:26]:
     with open(f"dictionaries/urban-dictionary/data/{char.upper()}.data", 'r', encoding='utf-8') as f:
         entries += f.read().replace('\n', ' ').replace('"', '')
@@ -20,16 +17,20 @@ Counter = Counter(words)
 print("Finding most common words...")
 top_words = Counter.items()
 urban = {}
-i = 0
-j = 0
-with open("dictionaries/urban.csv", 'w', encoding='utf-8') as f:
-    for word in top_words:
-        if (word[1] > 5) and (word[0].isalpha()):
-            urban[word[0]] = word[1]
-            j += 1
-            i += word[1]
-            f.write(f"{word[0]},{word[1]}\n")
+for word in top_words:
+    if (word[1] > 5) and (word[0].isalpha()):
+        urban[word[0]] = word[1]
 
 print(f"Urban dictionary is {len(urban)} words long")
+urban = {k: v for k, v in sorted(urban.items(), key=lambda item: item[1], reverse = True)}
+print(urban)
+i = 0
+j = 0
+file = "dictionaries/urban.csv"
+with open(file, 'w', encoding='utf-8') as f:
+    for word in urban.keys():
+        j += 1
+        i += urban[word]
+        f.write(f"{word},{urban[word]}\n")
 
-print(f"Outputted file with {j} terms with word frequencies out of {i}")
+print(f"Outputted file {file} with {j} terms with word frequencies out of {i}")
