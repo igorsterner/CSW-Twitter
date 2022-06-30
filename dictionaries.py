7ddf3e35ad76b37780322aa0b5f9ca5543777187
Igor Sterner <igorsterner@gmail.com>
2022-06-30 17:05:43 +0100

    dictionary formatting

--- dictionaries.py
+++ dictionaries.py
@@ -12,6 +12,49 @@ while True:
         maxInt = int(maxInt / 10)
 
 
+class CSWDictionary():
+
+    def __init__(self):
+        self.en = self.DictToList("en-dict.txt")
+        self.de = self.DictToList("de-dict.txt")
+        self.decc = self.DictToList("de-dictcc.txt")
+        self.ch = self.DictToList("de-ch-dict.txt")
+        self.at = self.DictToList("de-at-dict.txt")
+        self.urban = self.DictToList("urban-dict.txt")
+
+    def DictToList(self, file):
+        with open("dictionaries/" + file, 'r', encoding='utf-8') as f:
+            words = f.readlines()
+        return words
+
+    def CSWCombine(self):
+        en_tot = self.en + self.urban
+        de_tot = self.de + self.decc + self.ch + self.at
+
+        self.en_tot = set(en_tot)
+        self.de_tot = set(de_tot)
+
+        self.tot = self.en_tot - self.de_tot
+
+        with open('dictionaries/csw.txt', 'w', encoding='utf-8') as w:
+            for word in self.tot:
+                w.write(word)
+
+    def stats(self):
+        print(f"English: {len(self.en)}")
+        print(f"German: {len(self.decc)} + {len(self.de)}")
+        print(f"Austrian: {len(self.at)}")
+        print(f"Swiss: {len(self.ch)}")
+        print(f"Urban: {len(self.urban)}")
+
+        print(f"All German: {len(self.de_tot)}")
+        print(f"All English: {len(self.en_tot)}")
+
+        print(f"After removing identical words, English: {len(self.tot)}")
+        print(
+            f"Removed {len(self.en_tot) - len(self.tot)} words or {100 * (len(self.en_tot) - len(self.tot)) / len(self.en)}% of the words")
+
+
 class Dictionaries():
 
     def __init__(self, file_in, file_out):
@@ -109,73 +152,31 @@ class Dictionaries():
 
         print(f"Outputted file {self.file_out} with {len(self.words)} words")
 
-class CSWDictionary():
-
-    def __init__(self):
-        self.en = self.DictToList("en-dict.txt")
-        self.de = self.DictToList("de-dict.txt")
-        self.decc = self.DictToList("de-dictcc.txt")
-        self.ch = self.DictToList("de-ch-dict.txt")
-        self.at = self.DictToList("de-at-dict.txt")
-        self.urban = self.DictToList("urban-dict.txt")
-
-    def DictToList(self, file):
-        with open("dictionaries/"+file, 'r', encoding='utf-8') as f:
-            words = f.readlines()
-        return words
-
-    def CSWCombine(self):
-        en_tot = self.en + self.urban
-        de_tot = self.de + self.decc + self.ch + self.at
-
-        self.en_tot = set(en_tot)
-        self.de_tot = set(de_tot)
-
-        self.tot = self.en_tot - self.de_tot
-
-        with open('dictionaries/csw.txt', 'w', encoding='utf-8') as w:
-            for word in self.tot:
-                w.write(word)
-
-    def stats(self):
-        print(f"English: {len(self.en)}")
-        print(f"German: {len(self.decc)} + {len(self.de)}")
-        print(f"Austrian: {len(self.at)}")
-        print(f"Swiss: {len(self.ch)}")
-        print(f"Urban: {len(self.urban)}")
-
-        print(f"All German: {len(self.de_tot)}")
-        print(f"All English: {len(self.en_tot)}")
-
-        print(f"After removing identical words, English: {len(self.tot)}")
-        print(
-            f"Removed {len(self.en_tot) - len(self.tot)} words or {100 * (len(self.en_tot) - len(self.tot)) / len(self.en)}% of the words")
-
 
 if __name__ == "__main__":
-    # # English
-    # d = Dictionaries("100k-en-news-2020.txt", "en-dict.txt")
-    # d.make_dict('leipzig')
-    #
-    # # German
-    # d = Dictionaries("300k-de-news-2021.txt", "de-dict.txt")
-    # d.make_dict('leipzig')
-    #
-    # # German Dict.cc
-    # d = Dictionaries("dictdict.txt", "de-dictcc.txt")
-    # d.make_dict('dictcc')
-    #
-    # # Swiss
-    # d = Dictionaries("300k-de-ch-news-2012.txt", "de-ch-dict.txt")
-    # d.make_dict('leipzig')
-    #
-    # # Austrian
-    # d = Dictionaries("100k-de-at-news-2012.txt", "de-at-dict.txt")
-    # d.make_dict('leipzig')
-    #
-    # # Urban Dictionary
-    # d = Dictionaries(None, "urban-dict.txt")
-    # d.make_dict('urban')
+    # English
+    d = Dictionaries("100k-en-news-2020.txt", "en-dict.txt")
+    d.make_dict('leipzig')
+
+    # German
+    d = Dictionaries("300k-de-news-2021.txt", "de-dict.txt")
+    d.make_dict('leipzig')
+
+    # German Dict.cc
+    d = Dictionaries("dictdict.txt", "de-dictcc.txt")
+    d.make_dict('dictcc')
+
+    # Swiss
+    d = Dictionaries("300k-de-ch-news-2012.txt", "de-ch-dict.txt")
+    d.make_dict('leipzig')
+
+    # Austrian
+    d = Dictionaries("100k-de-at-news-2012.txt", "de-at-dict.txt")
+    d.make_dict('leipzig')
+
+    # Urban Dictionary
+    d = Dictionaries(None, "urban-dict.txt")
+    d.make_dict('urban')
 
     # CSW Dictionary
     c = CSWDictionary()
